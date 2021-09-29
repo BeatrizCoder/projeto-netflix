eb515142bb0998c3688f7517e212d474b7ca1110
Your Name <you@example.com>
2021-09-29 17:50:03 -0300

    file create dto and filmes controller in pogress

---
+++ src/filmes/dto/create-filme.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateFilmeDto{
+    @IsNotEmpty()
+  nome: string;
+
+    @IsOptional()
+    imagem:string;
+
+}
--- src/filmes/filmes.controller.ts
+++ src/filmes/filmes.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { 
+    Body,
+    Controller,
+    Get,
+    Post,
+    Delete,
+    Param,
+    Put,
+    ValidationPipe,
+    UsePipes,
+    ParseIntPipe,
+  
+ } from '@nestjs/common';
+ import { CreateFilmeDto } from './dto/create-filme.dto';
+ import { FilmesService } from './filmes.service';
+ import { Filme } from '.prisma/client';
 
-@Controller('filmes')
-export class FilmesController {}
+ @Controller('filmes')
+ export class FilmesController {
+   constructor(private filmesService: FilmesService) {}
+
+   @Get('/list')
+   @UsePipes(ValidationPipe)
+   async findMany(): Promise<Filme[]> {
+     return this.filmesService.getAll();
+   }
+
+ @Post('/create')
+ @Usepipes
